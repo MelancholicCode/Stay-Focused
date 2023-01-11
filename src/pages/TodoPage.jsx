9f25a07662247eba71277297494d48e7e0e871f6
DrlngLnr <madowl052@gmail.com>
2023-01-12 02:49:26 +0300

    Стилизовать тудушку под шапку

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,9 +1,11 @@
 import TasksContent from '../components/TasksContent/TasksContent';
+import Header from '../components/Header/Header';
 
 const TodoPage = () => {
 
   return (
     <div className="TodoPage">
+      <Header/>
       <div className="TodoPage__container">
         <TasksContent/>
       </div>
