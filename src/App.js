db90378cef7567daa695c8daebf203e936beaa8f
DrlngLnr <madowl052@gmail.com>
2023-01-12 20:03:27 +0300

    Разметить и стилизовать сайдбар

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Header/Header";
 import TodoPage from "./pages/TodoPage";
 
 function App() {
 
   return (
     <div className="App">
+      <Header/>
       <TodoPage/>
     </div>
   );
--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,12 +1,13 @@
 import TasksContent from '../components/TasksContent/TasksContent';
 import Header from '../components/Header/Header';
+import TodoSidebar from '../components/TodoSidebar/TodoSidebar';
 
 const TodoPage = () => {
 
   return (
     <div className="TodoPage">
-      <Header/>
       <div className="TodoPage__container">
+        <TodoSidebar/>
         <TasksContent/>
       </div>
     </div>
