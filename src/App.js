e05a6c3c3952db6fa9071fcfd63bd2568e3c2b62
DrlngLnr <madowl052@gmail.com>
2023-01-14 20:45:29 +0300

    Сделать редирект на страницу ошибки

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
+import ErrorPage from "./pages/ErrorPage";
 import TodoPage from "./pages/TodoPage";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/todo" replace />}/>
           <Route path="/todo/*" element={<TodoPage/>}/>
+          <Route path="/404" element={<ErrorPage/>}/>
+          <Route path="*" element={<Navigate to="/404" replace />}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import errorPic from '../img/404.png';
 
 const ErrorPage = () => {
   return (
-    <div className='ErrorPage'>
-      <p></p>
+    <div className='error-page'>
+      <div className="error-page__container">
+        <div className='error-page__content'>
+          <img className='error-page__picture' src={errorPic} alt="" />
+          <h1 className='error-page__title'>404</h1>
+          <p className='error-page__text'>Такой страницы не существует</p>
+        </div>
+      </div>
     </div>
   );
 };
